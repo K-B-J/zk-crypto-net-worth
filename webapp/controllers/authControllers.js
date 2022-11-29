c5ae3f5813fd877c47883b6e0783459d11e29eaf
Rajas-B <54888083+Rajas-B@users.noreply.github.com>
2022-11-29 21:13:52 +0530

    Added controllers for auth

---
+++ webapp/controllers/authControllers.js
@@ -0,0 +1,7 @@
+import expressAsyncHandler from "express-async-handler"
+
+const getDashboardData = expressAsyncHandler(async (req, res, next) => {})
+
+const setFeedsController = expressAsyncHandler(async (req, res, next) => {})
+
+export { getDashboardData, setFeedsController}
