f7ca0fe13b168014355a31dd36c12e6a241f7d90
Kevin Joshi <kevinjoshi46b@gmail.com>
2022-12-11 08:53:37 +0530

    feat: completed wallets functionality backend

---
+++ webapp/controllers/walletsControllers.js
@@ -0,0 +1,310 @@
+import expressAsyncHandler from "express-async-handler"
+import {
+    getPrimaryWalletAddress,
+    getSecondaryWalletAddresses,
+    removeSecondaryWalletAddress,
+    getPublicKey,
+    setSecondaryWalletAddress,
+} from "../utils/zKCryptoNetWorth.js"
+import { encrypt, decrypt } from "../utils/cryptography.js"
+import { ethers } from "ethers"
+
+const getWalletAddressesController = expressAsyncHandler(
+    async (req, res, next) => {
+        let walletAddresses = []
+        const fetchedPrimaryWalletAddress = await getPrimaryWalletAddress(
+            req.user.username.username
+        )
+        if (fetchedPrimaryWalletAddress.success) {
+            const primaryWalletAddress = decrypt(
+                req.user.username.privateKey,
+                fetchedPrimaryWalletAddress.result
+            )
+            walletAddresses.push({
+                type: "primary",
+                walletAddress: primaryWalletAddress,
+            })
+            const fetchedSecondaryWalletAddresses =
+                await getSecondaryWalletAddresses(req.user.username.username)
+            if (fetchedSecondaryWalletAddresses.success) {
+                fetchedSecondaryWalletAddresses.result.forEach(
+                    (walletAddress) => {
+                        walletAddresses.push({
+                            type: "secondary",
+                            walletAddress: decrypt(
+                                req.user.username.privateKey,
+                                walletAddress
+                            ),
+                            isLoading: false,
+                        })
+                    }
+                )
+                return res.status(200).json({
+                    message: "Wallet addresses fetched successfully!",
+                    data: walletAddresses,
+                })
+            } else {
+                return res.status(200).json({
+                    error: "Fetching secondary wallet addresses from blockchain failed",
+                    message: "Please try again",
+                })
+            }
+        } else {
+            return res.status(200).json({
+                error: "Fetching primary wallet address from blockchain failed",
+                message: "Please try again",
+            })
+        }
+    }
+)
+
+const setSecondaryWalletAddressController = expressAsyncHandler(
+    async (req, res, next) => {
+        const { message, signedMessage, secondaryWalletAddress } = req.body
+        if (message == undefined) {
+            return res.status(200).json({
+                error: "Data missing",
+                message: "Message is not provided",
+            })
+        }
+        if (signedMessage == undefined) {
+            return res.status(200).json({
+                error: "Data missing",
+                message: "Signed message is not provided",
+            })
+        }
+        if (secondaryWalletAddress == undefined) {
+            return res.status(200).json({
+                error: "Data missing",
+                message: "Secondary wallet address is not provided",
+            })
+        }
+        if (ethers.utils.isAddress(secondaryWalletAddress)) {
+            try {
+                const signerAddress = ethers.utils.verifyMessage(
+                    message,
+                    signedMessage
+                )
+                if (signerAddress == secondaryWalletAddress) {
+                    let walletAddresses = []
+                    const fetchedPrimaryWalletAddress =
+                        await getPrimaryWalletAddress(
+                            req.user.username.username
+                        )
+                    if (fetchedPrimaryWalletAddress.success) {
+                        const primaryWalletAddress = decrypt(
+                            req.user.username.privateKey,
+                            fetchedPrimaryWalletAddress.result
+                        )
+                        walletAddresses.push({
+                            type: "primary",
+                            walletAddress: primaryWalletAddress,
+                        })
+                        const fetchedSecondaryWalletAddresses =
+                            await getSecondaryWalletAddresses(
+                                req.user.username.username
+                            )
+                        if (fetchedSecondaryWalletAddresses.success) {
+                            fetchedSecondaryWalletAddresses.result.forEach(
+                                (walletAddress) => {
+                                    walletAddresses.push({
+                                        type: "secondary",
+                                        walletAddress: decrypt(
+                                            req.user.username.privateKey,
+                                            walletAddress
+                                        ),
+                                    })
+                                }
+                            )
+                            for (let i = 0; i < walletAddresses.length; i++) {
+                                if (
+                                    walletAddresses[i].walletAddress ==
+                                    secondaryWalletAddress
+                                ) {
+                                    if (walletAddresses[i].type === "primary") {
+                                        return res.status(200).json({
+                                            error: "Primary wallet cannot be relinked",
+                                            message:
+                                                "Please try linking another wallet",
+                                        })
+                                    } else {
+                                        return res.status(200).json({
+                                            error: "Secondary wallet has already been linked",
+                                            message:
+                                                "Please try linking another wallet",
+                                        })
+                                    }
+                                }
+                            }
+                            const fetchedPublicKey = await getPublicKey(
+                                req.user.username.username
+                            )
+                            if (fetchedPublicKey.success) {
+                                const encryptedSecondaryWalletAddress = encrypt(
+                                    fetchedPublicKey.result,
+                                    secondaryWalletAddress
+                                )
+                                const setSecondaryWalletAddressResult =
+                                    await setSecondaryWalletAddress(
+                                        req.user.username.username,
+                                        encryptedSecondaryWalletAddress
+                                    )
+                                if (setSecondaryWalletAddressResult.success) {
+                                    return res.status(200).json({
+                                        message:
+                                            "Secondary wallet linked successfully!",
+                                    })
+                                } else {
+                                    return res.status(200).json({
+                                        error: "Linking secondary wallet failed",
+                                        message: "Please try again",
+                                    })
+                                }
+                            } else {
+                                return res.status(200).json({
+                                    error: "Linking secondary wallet failed",
+                                    message: "Please try again",
+                                })
+                            }
+                        } else {
+                            return res.status(200).json({
+                                error: "Linking secondary wallet failed",
+                                message: "Please try again",
+                            })
+                        }
+                    } else {
+                        return res.status(200).json({
+                            error: "Linking secondary wallet failed",
+                            message: "Please try again",
+                        })
+                    }
+                } else {
+                    return res.status(200).json({
+                        error: "Invalid secondary wallet address",
+                        message:
+                            "The message was not signed by the secondary wallet address",
+                    })
+                }
+            } catch (error) {
+                return res.status(200).json({
+                    error: "Validating signature failed",
+                    message: "Please try again",
+                })
+            }
+        } else {
+            return res.status(200).json({
+                error: "Invalid secondary wallet address",
+                message:
+                    "The secondary wallet address provided is not a valid wallet address",
+            })
+        }
+    }
+)
+
+const removeSecondaryWalletAddressController = expressAsyncHandler(
+    async (req, res, next) => {
+        const { secondaryWalletAddress } = req.body
+        if (secondaryWalletAddress == undefined) {
+            return res.status(200).json({
+                error: "Data missing",
+                message: "Secondary wallet address is not provided",
+            })
+        }
+        if (ethers.utils.isAddress(secondaryWalletAddress)) {
+            let walletAddresses = []
+            const fetchedPrimaryWalletAddress = await getPrimaryWalletAddress(
+                req.user.username.username
+            )
+            if (fetchedPrimaryWalletAddress.success) {
+                const primaryWalletAddress = decrypt(
+                    req.user.username.privateKey,
+                    fetchedPrimaryWalletAddress.result
+                )
+                walletAddresses.push({
+                    type: "primary",
+                    walletAddress: primaryWalletAddress,
+                })
+                const fetchedSecondaryWalletAddresses =
+                    await getSecondaryWalletAddresses(
+                        req.user.username.username
+                    )
+                if (fetchedSecondaryWalletAddresses.success) {
+                    fetchedSecondaryWalletAddresses.result.forEach(
+                        (walletAddress) => {
+                            walletAddresses.push({
+                                type: "secondary",
+                                walletAddress: decrypt(
+                                    req.user.username.privateKey,
+                                    walletAddress
+                                ),
+                                encryptedWalletAddress: walletAddress,
+                            })
+                        }
+                    )
+                    let encrptedWalletAddress = undefined
+                    for (let i = 0; i < walletAddresses.length; i++) {
+                        if (
+                            walletAddresses[i].walletAddress ==
+                            secondaryWalletAddress
+                        ) {
+                            if (walletAddresses[i].type == "primary") {
+                                return res.status(200).json({
+                                    error: "Primary wallet cannot be unlinked",
+                                    message:
+                                        "Please try unlinking secondary wallets",
+                                })
+                            } else {
+                                encrptedWalletAddress =
+                                    walletAddresses[i].encryptedWalletAddress
+                            }
+                        }
+                    }
+                    if (encrptedWalletAddress == undefined) {
+                        return res.status(200).json({
+                            error: "Secondary wallet address provided is not linked",
+                            message:
+                                "Please try unlinking other linked secondary wallet addresses",
+                        })
+                    }
+                    const removeSecondaryWalletAddressResult =
+                        await removeSecondaryWalletAddress(
+                            req.user.username.username,
+                            encrptedWalletAddress
+                        )
+                    if (removeSecondaryWalletAddressResult.success) {
+                        return res.status(200).json({
+                            message: "Secondary wallet unlinked successfully!",
+                        })
+                    } else {
+                        return res.status(200).json({
+                            error: "Unlinking secondary wallet failed",
+                            message: "Please try again",
+                        })
+                    }
+                } else {
+                    return res.status(200).json({
+                        error: "Unlinking secondary wallet failed",
+                        message: "Please try again",
+                    })
+                }
+            } else {
+                return res.status(200).json({
+                    error: "Unlinking secondary wallet failed",
+                    message: "Please try again",
+                })
+            }
+        } else {
+            return res.status(200).json({
+                error: "Invalid secondary wallet address",
+                message:
+                    "The secondary wallet address provided is not a valid wallet address",
+            })
+        }
+    }
+)
+
+export {
+    getWalletAddressesController,
+    setSecondaryWalletAddressController,
+    removeSecondaryWalletAddressController,
+}
---
+++ webapp/utils/zKCryptoNetWorth.js
@@ -0,0 +1,226 @@
+import { ethers } from "ethers"
+import { ALCHEMY_GOERLI, WALLET_PRIVATE_KEY } from "../env.js"
+import zKCryptoNetWorthGoerli from "./contracts/zKCryptoNetWorthGoerli.json" assert { type: "json" }
+
+const provider = new ethers.providers.JsonRpcProvider(ALCHEMY_GOERLI)
+const providerContract = new ethers.Contract(
+    zKCryptoNetWorthGoerli.address,
+    zKCryptoNetWorthGoerli.abi,
+    provider
+)
+const wallet = new ethers.Wallet(WALLET_PRIVATE_KEY, provider)
+const signerContract = providerContract.connect(wallet)
+
+const isUniquePublicKey = async (publicKey) => {
+    try {
+        const result = await providerContract.isUniquePublicKey(publicKey)
+        return { success: true, result }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const isUniqueUsername = async (username) => {
+    try {
+        const result = await providerContract.isUniqueUsername(username)
+        return { success: true, result }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const setAccount = async (username, publicKey, primaryWalletAddress) => {
+    try {
+        const result = await signerContract.setAccount(
+            username,
+            publicKey,
+            primaryWalletAddress
+        )
+        await result.wait(1)
+        return { success: true }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const getAccount = async (username) => {
+    try {
+        const result = await providerContract.getAccount(username)
+        return { success: true, result }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const getPublicKey = async (username) => {
+    try {
+        const result = await providerContract.getPublicKey(username)
+        return { success: true, result }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const getPrimaryWalletAddress = async (username) => {
+    try {
+        const result = await providerContract.getPrimaryWalletAddress(username)
+        return { success: true, result }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const setSecondaryWalletAddress = async (username, secondaryWalletAddress) => {
+    try {
+        const result = await signerContract.setSecondaryWalletAddress(
+            username,
+            secondaryWalletAddress
+        )
+        await result.wait(1)
+        return { success: true }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const removeSecondaryWalletAddress = async (
+    username,
+    secondaryWalletAddress
+) => {
+    try {
+        const result = await signerContract.removeSecondaryWalletAddress(
+            username,
+            secondaryWalletAddress
+        )
+        await result.wait(1)
+        return { success: true }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const getSecondaryWalletAddresses = async (username) => {
+    try {
+        const result = await providerContract.getSecondaryWalletAddresses(
+            username
+        )
+        return { success: true, result }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const setRequestMetadata = async (
+    id,
+    sender,
+    receiver,
+    threshold,
+    status,
+    reqResult,
+    proof
+) => {
+    try {
+        const result = await signerContract.setRequestMetadata(
+            id,
+            sender,
+            receiver,
+            threshold,
+            status,
+            reqResult,
+            proof
+        )
+        await result.wait(1)
+        return { success: true }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const getLatestId = async () => {
+    try {
+        const result = await signerContract.getLatestId()
+        return { success: true, result }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const getRequestMetadata = async (id) => {
+    try {
+        const result = await providerContract.getRequestMetadata(id)
+        return { success: true, result }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const getRequestMetadatas = async (ids) => {
+    try {
+        const result = await providerContract.getRequestMetadatas(ids)
+        return { success: true, result }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const setRequests = async (sender, senderId, receiver, receiverId) => {
+    try {
+        const result = await signerContract.setRequests(
+            sender,
+            senderId,
+            receiver,
+            receiverId
+        )
+        await result.wait(1)
+        return { success: true }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const getRequests = async (username) => {
+    try {
+        const result = await providerContract.getRequests(username)
+        return { success: true, result }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const getIncomingRequests = async (username) => {
+    try {
+        const result = await providerContract.getIncomingRequests(username)
+        return { success: true, result }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+const getOutgoingRequests = async (username) => {
+    try {
+        const result = await providerContract.getOutgoingRequests(username)
+        return { success: true, result }
+    } catch (error) {
+        return { success: false, error }
+    }
+}
+
+export {
+    isUniquePublicKey,
+    isUniqueUsername,
+    setAccount,
+    getAccount,
+    getPublicKey,
+    getPrimaryWalletAddress,
+    setSecondaryWalletAddress,
+    removeSecondaryWalletAddress,
+    getSecondaryWalletAddresses,
+    setRequestMetadata,
+    getLatestId,
+    getRequestMetadata,
+    getRequestMetadatas,
+    setRequests,
+    getRequests,
+    getIncomingRequests,
+    getOutgoingRequests,
+}
