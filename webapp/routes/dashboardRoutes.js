aa3da56ef503acf1976fdacf62ed26201bc0b1b5
Kaushal <kaushal.binjola11@gmail.com>
2023-02-13 15:50:59 +0530

    changes to dashboard routes

--- webapp/routes/dashboardRoutes.js
+++ webapp/routes/dashboardRoutes.js
@@ -8,8 +8,8 @@ import { protect, admin } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
-router.route("/price_coins").get(protect, getPricesController)
-router.route("/quantity_coins").get(protect, getQuantityController)
+router.route("/price").get(protect, getPricesController)
+router.route("/quantity").get(protect, getQuantityController)
 router.route("/feeds").post(protect, admin, setFeedsController)
 
 export default router
